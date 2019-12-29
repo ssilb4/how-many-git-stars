8a42fe33b942ab40abd27eefac4dc4b2902c3ffe
ssilb4 <himchan@DESKTOP-30N2U0I.localdomain>
2019-12-29 19:47:26 +0900

    add async and can get several usernames

--- src/gitapi/core.clj
+++ src/gitapi/core.clj
@@ -8,6 +8,8 @@
 (def repos "/repos")
 
 (def progress (atom 0))
+(def index    (atom -1))
+(def result   (atom []))
 (def _0Bar   "○○○○○   0%")
 (def _20Bar  "●○○○○  20%")
 (def _40Bar  "●●○○○  40%")
@@ -16,37 +18,41 @@
 (def _100Bar "●●●●● 100%")
 
 (defn printProgressBar
-  [value]
-  (case value
+  [currentValue beforeValue]
+  (case currentValue
     0   (do (println _0Bar)   (reset! progress 20))
     20  (do (println _20Bar)  (reset! progress 40))
     40  (do (println _40Bar)  (reset! progress 60))
     60  (do (println _60Bar)  (reset! progress 80))
-    80  (do (println _80Bar))
+    80  (do (println _80Bar)  (reset! progress 100))
     100 (do (println _100Bar))))
 
 (add-watch progress :progress
   (fn [key atom old-state new-state]
-    (printProgressBar new-state)))
+    (printProgressBar new-state old-state)))
 
 
 (defn jsonParser
-  [json]
-    (reduce + (map #(Integer/parseInt %) (take-nth 2 (rest (str/split (str json) #"stargazers_count\\\":|,\\\"watchers_count\\\":"))))))
+  [json index]
+    (swap! result assoc index (reduce + (map #(Integer/parseInt %) (take-nth 2 (rest (str/split (str json) #"stargazers_count\\\":|,\\\"watchers_count\\\":"))))))
+    (println @result))
 
 (defn getStars
-  [name]
+  [name index]
     (do
       (println (str "url: " completeURL name repos))
       (http/get (str completeURL name repos) {:async? true}
-        (fn [response]  (do (reset! progress 100) 
-                            (println (jsonParser (str response)))))
+        (fn [response]  (jsonParser (str response) index))
         (fn [exception] (println "exception message is: " (.getMessage exception))))))
 
 (defn -main
   "Production"
   [& args]
+    (reset! result (vec args))
+    (println @result)
     (if (nil? args)
       (println "Usage: lein run [user-id]")
-      (do (getStars (str (first args)))
+      (do (dorun (map (fn [value] 
+                          (swap! index inc) 
+                          (getStars value @index)) args))
           (reset! progress 20))))
